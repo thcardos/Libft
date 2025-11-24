d5214bee67077d217f71c568abf980b79028ba0c
thcardos <thcardos@student.42malaga.com>
2025-11-24 19:41:18 +0100

    Add files via upload

---
+++ Makefile
@@ -0,0 +1,39 @@
+NAME = libft.a
+
+CC = cc
+CFLAGS = -Wall -Wextra -Werror
+
+SOURCES = ft_isalpha.c \
+		ft_isdigit.c \
+		ft_isalnum.c \
+		ft_isascii.c \
+		ft_isprint.c \
+		ft_strlen.c \
+		ft_memset.c \
+		ft_bzero.c \
+		ft_memcpy.c \
+		ft_memmove.c \
+		
+
+OBJECTS = $(SOURCES:.c=.o)
+
+
+%.o: %.c
+	$(CC) $(CFLAGS) -c $< -o $@
+	
+all: $(NAME)
+
+$(NAME): $(OBJECTS)
+	ar rc $(NAME) $(OBJECTS)
+	ranlib $(NAME)
+
+
+clean:
+	rm -f $(OBJECTS)
+
+fclean: clean
+	rm -f $(NAME)
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ ft_memmove.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thcardos <thcardos@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/20 15:24:16 by thcardos          #+#    #+#             */
+/*   Updated: 2025/11/20 15:24:21 by thcardos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void    *ft_memmove(void *dest, const void *src, size_t n)
+{
+    unsigned char   *a;
+    unsigned char   *b;
+    size_t  i;
+    if (!src && !dest)
+    {
+        return (NULL);
+    }
+    a = (unsigned char*)dest;
+    b = (unsigned char*)src;
+    i = 0;
+
+    if(a < b)
+    {
+        while (i < n)
+        {
+            a[i] = b[i];
+            i++;
+        }
+    }
+    else
+    {
+        i = n;
+        while(i > 0)
+        {
+            i--;
+            a[i] = b[i];
+        }
+    }
+    return (dest);
+}
